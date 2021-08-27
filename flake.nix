e47a44f1fd4babd67cbde7043522ab0a732eefef
Gregor Pogacnik <1640719+fiksn@users.noreply.github.com>
2021-08-27 11:18:43 +0200

    Explain mkMachine

--- flake.nix
+++ flake.nix
@@ -9,21 +9,21 @@
   outputs = { self, flake-utils, nixpkgs }:
     with nixpkgs;
     let
-      pkgs = import nixpkgs {
-        overlays = [
-          self.overlay
-        ];
-        inherit system;
-      };
-
       getNixFilesInDir = dir: builtins.filter (file: lib.hasSuffix ".nix" file && file != "default.nix") (builtins.attrNames (builtins.readDir dir));
       genKey = str: lib.replaceStrings [ ".nix" ] [ "" ] str;
-      genModValue = dir: str: {  config, options, lib, ... }: { imports = [ "/${dir}/${str}" ]; };
-      genMachineValue = dir: str: lib.nixosSystem { system = "aarch64-linux"; modules = [ "/${dir}/${str}" ]; };
+      genModValue = dir: str: { config, options, lib, ... }: { imports = [ (dir + "/${str}") ]; };
+      genMachineValue = dir: str: mkMachine "aarch64-linux" (dir + "/${str}");
       oneFrom = gen: dir: str: { "${genKey str}" = gen dir str; };
 
       modulesFromDir = dir: builtins.foldl' (x: y: x // (oneFrom genModValue dir y)) { } (getNixFilesInDir dir);
       machinesFromDir = dir: builtins.foldl' (x: y: x // (oneFrom genMachineValue dir y)) { } (getNixFilesInDir dir);
+
+      # A cursed wrapper around lib.nixosSystem - trick is that you can't pass parameters easily to NixOS modules, so I've used this
+      # https://stackoverflow.com/questions/47650857/nixos-module-imports-with-arguments#answer-58055106 idea
+      mkMachine = arch: file: lib.nixosSystem {
+        system = arch;
+        modules = [ ({ pkgs, lib, config, options, ... } @ args: { nixpkgs.overlays = [ self.overlay ]; imports = [ (import file (args // { inherit self; })) ]; }) ];
+      };
     in
     {
       # Overlays
@@ -38,10 +38,7 @@
       # Machines (just demo, others are in private repo)
       nixosConfigurations = machinesFromDir ./machines //
       {
-        vpsNode = lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [ ./machines/node.nix ];
-        };
+        vpsNode = mkMachine "x86_64-linux" ./machines/node.nix ];
       };
     } //
 
