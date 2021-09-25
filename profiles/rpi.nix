d410af7fb6ea3ebe5dd0ba1f672a76eb702fc44a
Gregor Pogacnik <1640719+fiksn@users.noreply.github.com>
2021-09-25 21:32:20 +0200

    More rpi stuff

--- profiles/rpi.nix
+++ profiles/rpi.nix
@@ -1,9 +1,22 @@
 { config, pkgs, lib, ... }:
 {
   hardware.enableRedistributableFirmware = true;
+  hardware.raspberry-pi."4".fkms-3d.enable = true;
+  powerManagement.cpuFreqGovernor = "ondemand";
 
   boot = {
     kernelPackages = pkgs.linuxPackages_rpi4;
+    tmpOnTmpfs = true;
+    initrd.availableKernelModules = [ "usbhid" "usb_storage" ];
+    # ttyAMA0 is the serial console broken out to the GPIO
+    kernelParams = [
+      "8250.nr_uarts=1"
+      "console=ttyAMA0,115200"
+      "console=tty1"
+      # Some gui programs need this
+      "cma=128M"
+    ];
+
     loader = {
       grub.enable = false;
       # Silence the error
