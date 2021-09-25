4ca1445dd626ae77907bac744a33d26abad00e24
Gregor Pogacnik <1640719+fiksn@users.noreply.github.com>
2021-09-25 17:34:34 +0200

    typo

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -21,32 +21,33 @@ in
       ];
       sha256 = "sha256-yv8jRJIgz0V1PzEs7+3lOp6sZAALswB5eRZSYja2oeA=";
     };
+  });
 
-    bitcoind-22 = bitcoind.overrideAttrs (old: rec {
-      version = "22.0";
-      src = fetchurl {
-        urls = [
-          "https://bitcoincore.org/bin/bitcoin-core-${version}/bitcoin-${version}.tar.gz"
-        ];
-        sha256 = "d0e9d089b57048b1555efa7cd5a63a7ed042482045f6f33402b1df425bf9613b";
-      };
-    });
+  bitcoind-22 = bitcoind.overrideAttrs (old: rec {
+    version = "22.0";
+    src = fetchurl {
+      urls = [
+        "https://bitcoincore.org/bin/bitcoin-core-${version}/bitcoin-${version}.tar.gz"
+      ];
+      sha256 = "d0e9d089b57048b1555efa7cd5a63a7ed042482045f6f33402b1df425bf9613b";
+    };
+  });
 
-    bitcoind-unstable = pkgs-unstable.bitcoind;
+  bitcoind-unstable = pkgs-unstable.bitcoind;
 
-    aperture = callPackage ./aperture { };
+  aperture = callPackage ./aperture { };
 
-    # Fix broken platforms
-    btcpayserver = pkgs-unstable.btcpayserver.overrideAttrs (attrs: {
-      meta = attrs.meta // { platforms = intersectAll (depWithOrig ([ attrs.meta.platforms or lib.platforms.linux ]) (attrs.nativeBuildInputs or [ ])); };
-    });
-    nbxplorer = pkgs-unstable.nbxplorer.overrideAttrs (attrs: {
-      meta = attrs.meta // { platforms = intersectAll (depWithOrig ([ attrs.meta.platforms or lib.platforms.linux ]) (attrs.nativeBuildInputs or [ ])); };
-    });
+  # Fix broken platforms
+  btcpayserver = pkgs-unstable.btcpayserver.overrideAttrs (attrs: {
+    meta = attrs.meta // { platforms = intersectAll (depWithOrig ([ attrs.meta.platforms or lib.platforms.linux ]) (attrs.nativeBuildInputs or [ ])); };
+  });
+  nbxplorer = pkgs-unstable.nbxplorer.overrideAttrs (attrs: {
+    meta = attrs.meta // { platforms = intersectAll (depWithOrig ([ attrs.meta.platforms or lib.platforms.linux ]) (attrs.nativeBuildInputs or [ ])); };
+  });
 
-    lightning-loop = callPackage ./lightning-loop { };
-    lightning-pool = callPackage ./lightning-pool { };
-    balanceofsatoshis = (callPackage ./balanceofsatoshis/override.nix { }).package;
+  lightning-loop = callPackage ./lightning-loop { };
+  lightning-pool = callPackage ./lightning-pool { };
+  balanceofsatoshis = (callPackage ./balanceofsatoshis/override.nix { }).package;
 
-    #lightning-terminal = callPackage ./lightning-terminal { };
-  }
+  #lightning-terminal = callPackage ./lightning-terminal { };
+}
