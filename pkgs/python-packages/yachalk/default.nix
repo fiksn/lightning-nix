8ae56300d1730b7332045f2aee3061a45323a07b
Gregor Pogacnik <1640719+fiksn@users.noreply.github.com>
2021-12-16 22:00:17 +0100

    Added rebalance-lnd

---
+++ pkgs/python-packages/yachalk/default.nix
@@ -0,0 +1,21 @@
+{ lib, buildPythonPackage, fetchPypi, importlib-resources }:
+buildPythonPackage rec {
+  pname = "yachalk";
+  version = "0.1.5";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "12krk729xmrn9wms0wn3zzarvkdf0hb2zs7xml30ya4a35mbdhya";
+  };
+
+  nativeBuildInputs = [
+    importlib-resources
+  ];
+
+  doCheck = false;
+
+  meta = with lib; {
+    description = "Yet Another Chalk - terminal string styling done right";
+    homepage = "https://github.com/bluenote10/yachalk";
+  };
+}
