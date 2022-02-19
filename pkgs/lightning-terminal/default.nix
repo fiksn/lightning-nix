34e16ced181fc0226dd5e97c0912b8e8dca07fa8
Gregor Pogačnik <1640719+fiksn@users.noreply.github.com>
2022-02-19 14:16:34 +0100

    Lightning terminal: 0.6.1 -> 0.6.2

--- pkgs/lightning-terminal/default.nix
+++ pkgs/lightning-terminal/default.nix
@@ -6,16 +6,16 @@
 
 buildGoModule rec {
   pname = "lightning-terminal";
-  version = "0.6.1-alpha";
+  version = "0.6.2-alpha";
 
   src = fetchFromGitHub {
     owner = "lightninglabs";
     repo = "lightning-terminal";
     rev = "v${version}";
-    sha256 = "0434girh41zd15gpwkb721pzjnx2ay02yhwsl46sxaq7yjwnp37b";
+    sha256 = "0v1byxcnj0da64yyql2hb1ylwlfq482nkm2l1bj3l9imbcxgwjcj";
   };
 
-  vendorSha256 = "1c9i255csq80ygpsjgl5fh858bwzazynfw98629fvy2nc2blywkl";
+  vendorSha256 = "1l04q7xww4g61ki3zd9cwj62k9q9b5wdn9g3cmj3j3cvk7ncah9h";
 
   subPackages = [ "cmd/litcli" "cmd/litd" ];
 
@@ -25,27 +25,39 @@ buildGoModule rec {
     packageJSON = "${src}/app/package.json";
     yarnLock = "${src}/app/yarn.lock";
 
-    doDist = false;
-    buildPhase = ''
-      runHook preBuild
-      yarn --offline build
-      runHook postBuild
-    '';
+    #doDist = false;
+    distPhase = "true";
 
     installPhase = ''
       runHook preInstall
-      ls -ali || true
-      ls -ali build || true
-      mv build $out
+
+      # Since the stupid app uses ../node_modules (and relative paths are off)
+      sed -i "s|@import '../node_modules/|@import '../../../node_modules/|g" /build/app/deps/lightning-terminal/src/App.scss
+
+      # Stupid .cache folder in node_modules needs to be writable
+      shopt -s dotglob
+      ORIG=$(readlink /build/app/deps/lightning-terminal/node_modules)
+      rm /build/app/deps/lightning-terminal/node_modules
+      mkdir /build/app/deps/lightning-terminal/node_modules
+      cp -a $ORIG/* /build/app/deps/lightning-terminal/node_modules
+      shopt -u dotglob
+
+      # Or else you get even more issues with missing .eslintrc
+      export DISABLE_ESLINT_PLUGIN=true
+
+      # The actual build
+      yarn --offline build
+
+      mkdir -p $out
+      cp -a /build/app/deps/lightning-terminal/build/* $out
+
       runHook postInstall
     '';
   };
 
   postPatch = ''
     mkdir -p app/build
-    echo ${frontend}
-    find ${frontend}
-    #cp ${frontend}/* app/build
+    cp -r ${frontend}/* app/build
   '';
 
   meta = with lib; {
